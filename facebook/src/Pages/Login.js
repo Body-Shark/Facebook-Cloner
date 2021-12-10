5d17252cdabf4cf71250c60baa8945a3ffd296e5
Fernando Nunes <fernandonunesjs@hotmail.com>
2021-12-10 03:24:16 -0300

    End profile box

--- facebook/src/Pages/Login.js
+++ facebook/src/Pages/Login.js
@@ -7,7 +7,6 @@ import InputPasswordComponent from "../Components/FormComponents/InputPassword";
 import ButtonComponent from "../Components/FormComponents/ButtonSubmit";
 
 class Login extends Component {
-
   render() {
     const notify = 5;
     return (
@@ -22,19 +21,22 @@ class Login extends Component {
               </p>
 
               <div className="flex gap-3">
-                <div className="w-40 border rounded-md mt-6 bg-white">
-                  <div className='w-5 h-5 bg-red-550 rounded-full float-right z-10 -right-2 -top-2 relative text-center text-white items-center flex justify-center text-xs'>{notify}</div>
+                <div className="w-40 border rounded-md mt-6 bg-white cursor-pointer hover:scale-125">
+                  <div className="w-4 h-4 bg-gray-200 opacity-70 left-1 top-1 rounded-full float-left z-10 relative text-center text-gray-700 items-center flex justify-center text-xs hover:scale-125">x</div>
+                  <div className="w-5 h-5 bg-red-550 rounded-full float-right z-10 -right-2 -top-2 relative text-center text-white items-center flex justify-center text-xs">
+                    {notify}
+                  </div>
                   <img
                     src="https://scontent.fcxj5-1.fna.fbcdn.net/v/t1.6435-1/p160x160/69618903_433613227496947_3470654954685857792_n.jpg?_nc_cat=106&amp;ccb=1-5&amp;_nc_sid=dbb9e7&amp;_nc_ohc=75Vo4x3hYncAX8zkHYr&amp;_nc_ht=scontent.fcxj5-1.fna&amp;oh=c184bc5926e7e0e577087a17b14eaeb8&amp;oe=61B75A00"
                     alt=""
                     label="Fernando Nunes"
                     className="rounded-md rounded-b-none relative -top-5"
                   />
-                  <div className="text-center py-1.5 text-lg text-gray-500">
+                  <div className="text-center py-1.5 text-lg text-gray-500 relative -top-5">
                     Fernando
                   </div>
                 </div>
-                <div className="w-40 border rounded-lg mt-6 bg-white">
+                <div className="w-40 border rounded-lg mt-6 bg-white cursor-pointer hover:scale-125">
                   <div className="w-full h-40 rounded-md rounded-b-none bg-gray-100 flex items-center justify-center">
                     <img src={PlusImg} alt="" />
                   </div>
@@ -77,27 +79,26 @@ class Login extends Component {
             </div>
           </div>
         </div>
-        <div className='w-full grid justify-center items-center'>
+        <div className="w-full grid justify-center items-center">
           <div className="px-32 py-5">
             <p className="text-gray-500 pl-4 text-xs mb-2">
-              <a href="">Português (Brasil)</a> <a href="">English (US)</a> <a href="">Español Français (France)</a> <a href="">Italiano</a>
-              <a href="">Deutsch</a> <a href="">العربية</a> <a href="">हिन्दी 中文(简体</a>) <a href="">日本語</a>
-              
+              <a href="">Português (Brasil)</a> <a href="">English (US)</a>{" "}
+              <a href="">Español Français (France)</a> <a href="">Italiano</a>
+              <a href="">Deutsch</a> <a href="">العربية</a>{" "}
+              <a href="">हिन्दी 中文(简体</a>) <a href="">日本語</a>
             </p>
             <hr />
           </div>
-          <div className='px-32'>
-            <p className='text-gray-500 text-xs text-justify '>
+          <div className="px-32">
+            <p className="text-gray-500 text-xs text-justify ">
               Cadastre-seEntrarMessengerFacebook
               LiteWatchLocaisJogosMarketplaceFacebook PayVagas de
-              empregoOculusPortalInstagramBoletimLocalCampanhas de arrecadação de
-              fundosServiçosCentral de Informações de VotaçãoGruposSobreCriar
-              anúncioCriar PáginaDesenvolvedoresCarreirasPrivacidadeCookiesEscolhas
-              para anúnciosTermosAjudaConfiguraçõesRegistro de Atividades 
-              <p className='py-4'>
-                Meta ©
-                2021
-              </p>
+              empregoOculusPortalInstagramBoletimLocalCampanhas de arrecadação
+              de fundosServiçosCentral de Informações de VotaçãoGruposSobreCriar
+              anúncioCriar
+              PáginaDesenvolvedoresCarreirasPrivacidadeCookiesEscolhas para
+              anúnciosTermosAjudaConfiguraçõesRegistro de Atividades
+              <p className="py-4">Meta © 2021</p>
             </p>
           </div>
         </div>
